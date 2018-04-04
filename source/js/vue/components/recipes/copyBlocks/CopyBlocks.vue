de61198fd003db0c0584b96ddaf7b7a2f45f14a5
Ben Gauslin <ben@gauslin.com>
2018-04-04 19:02:01 -0400

    CopyBlocks refactoed as a single component

--- source/js/vue/components/recipes/copyBlocks/CopyBlocks.vue
+++ source/js/vue/components/recipes/copyBlocks/CopyBlocks.vue
@@ -1,35 +1,55 @@
 <template lang="pug">
   div
-    template(v-for="block in data")
-      blurb-with-heading(
-        v-if="block.type === 'blurbWithHeading'",
-        :heading="block.heading",
-        :blurb="block.blurb"
+    div.copy-block(
+      v-if="data.type === 'blurbWithHeading'",
+    )
+      h4 {{ data.heading }}
+      div(
+        v-html="data.blurb",
       )
-      div.blurb(
-        v-if="block.type === 'blurb'",
-        v-html="block.blurb",
-      )
-      list(
-        v-if="block.type === 'list'",
-        :list="block.list"
-      )
+
+    div.blurb(
+      v-if="data.type === 'blurb'",
+      v-html="data.blurb",
+    )
+
+    ul.blurb-list(
+      v-if="data.type === 'list'",
+    )
+      li(
+        v-for="item in data.list",
+      ) {{ item }}
+
 </template>
 
 <script>
-// TODO: consolidate all copyBlocks components
-
-import BlurbWithHeading from './BlurbWithHeading.vue'
-import List from './List.vue'
-
 export default {
-  components: { 
-    BlurbWithHeading,
-    List,
-  },
-
-  props: [
-    'data'
-  ]
+  props: ['data']
 }
 </script>
+
+<style lang="stylus">
+@import '../../../../../stylus/_config/'
+
+.copy-block
+  padding .5rem 0
+
+  h4
+    font_sans_heavy()
+    margin 0 0 .25em
+    small_caps()
+
+  p
+    font_sans()
+    font-size px_to_em(14)
+
+.blurb-list
+  margin 0 0 1em
+
+  li
+    font_sans()
+    font-size px_to_em(14)
+    list-style none
+    margin 0 0 .5em
+
+</style>
