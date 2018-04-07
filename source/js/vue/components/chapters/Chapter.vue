2c768475e5a397e4ec58d18bdcdc1c956956d79c
Ben Gauslin <ben@gauslin.com>
2018-04-07 12:51:35 -0400

    BEM for chapter elements

--- source/js/vue/components/chapters/Chapter.vue
+++ source/js/vue/components/chapters/Chapter.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div.chapter
-    div.chapter-content
-      div.intro
+    div.chapter__content
+      div.chapter__intro
         basic-cover(
           :blurb="data.blurb",
           :count="data.recipeCount + ' Recipes'",
@@ -37,48 +37,48 @@ export default {
 @import '../../../../stylus/_config/'
 
 .chapter
-  .chapter-content
-    background WHITE
-    margin 0 auto
-
-  .intro
-    overflow hidden
-
   @media(min-width BREAKPOINT_SMALL)
     margin 1.5rem MARGINS_SMALL 0
-
-    .chapter-content
-      max-width STACKED_WIDTH
 
   @media(min-width BREAKPOINT_MEDIUM)
     margin 2.25rem MARGINS_MEDIUM 0
 
-    .chapter-content
-      max-width none
-      width STACKED_WIDTH
-
   @media(min-width BREAKPOINT_LARGE)
     margin 3rem MARGINS_LARGE 0
-
-    .chapter-content
-      overflow hidden
-      padding-bottom 50%
-      position relative
-      width 100%
-
-      .intro
-        position_it(absolute, 0, null, null, 0)
-        width 50%
-
-      .previews
-        height 100%
-        overflow-y scroll
-        -webkit-overflow-scrolling touch
-        position_it(absolute, 0, 0, null, null)
-        width 50%
 
   @media(min-width BREAKPOINT_XLARGE)
     margin 3rem auto 0
     width WIDTH_XLARGE
+
+.chapter__content
+  background WHITE
+  margin 0 auto
+  
+  @media(min-width BREAKPOINT_SMALL)
+    max-width STACKED_WIDTH
+
+  @media(min-width BREAKPOINT_MEDIUM)
+    max-width none
+    width STACKED_WIDTH
+
+  @media(min-width BREAKPOINT_LARGE)
+    overflow hidden
+    padding-bottom 50%
+    position relative
+    width 100%
+    
+    .previews
+      height 100%
+      overflow-y scroll
+      -webkit-overflow-scrolling touch
+      position_it(absolute, 0, 0, null, null)
+      width 50%
+
+.chapter__intro
+  overflow hidden
+
+  @media(min-width BREAKPOINT_LARGE)
+    position_it(absolute, 0, null, null, 0)
+    width 50%
 
 </style>
