01458fcb35a2eb93b8e45306592139daa0ca0ac6
Ben Gauslin <ben@gauslin.com>
2018-04-07 08:47:35 -0400

    refactored/cleaned-up App.vue

--- source/js/vue/components/App.vue
+++ source/js/vue/components/App.vue
@@ -1,32 +1,42 @@
 <template lang="pug">
   div.site
-    app-header(:parent="parent", :home="home")
+    app-header(
+      :home="home",
+      :parent="parent",
+    )
     div.content
-      preloader(v-if="!dataLoaded")
+      preloader(
+        v-if="!dataLoaded",
+      )
       transition(
         @before-enter="beforeEnter",
         @after-enter="afterEnter",
         @before-leave="beforeLeave",
         @after-leave="afterLeave",
-        mode="out-in"
+        mode="out-in",
       )
         router-view(
           v-if="dataLoaded",
           :data="data",
-          :key="data"
+          :key="data",
         )
     app-footer
 </template>
 
 <script>
-import AppFooter from './global/Footer.vue'
-import AppHeader from './global/Header.vue'
-import Breadcrumbs from './partials/Breadcrumbs.vue'
-import Preloader from './partials/Preloader.vue'
-import request from 'superagent'
+import AppFooter from './global/Footer.vue';
+import AppHeader from './global/Header.vue';
+import Breadcrumbs from './partials/Breadcrumbs.vue';
+import Preloader from './partials/Preloader.vue';
+import request from 'superagent';
 
 export default {
-  components: { AppFooter, AppHeader, Breadcrumbs, Preloader },
+  components: {
+    AppFooter,
+    AppHeader,
+    Breadcrumbs,
+    Preloader,
+  },
 
   data () {
     return {
@@ -35,161 +45,166 @@ export default {
       endpoint: '',
       home: null,
       transitionEnter: null,
-      transitionLeave: null
+      transitionLeave: null,
     }
   },
 
   computed: {
     parent () {
-      return this.$store.getters.getParent
+      return this.$store.getters.getParent;
     },
+
     direction () {
-      return this.$store.getters.getDirection
-    }
+      return this.$store.getters.getDirection;
+    },
   },
 
   created () {
-    this.isHome()
-    this.isSearch()
+    this.isHome();
+    this.isSearch();
 
-    this.endpoint = this.$route.meta.endpoint
+    this.endpoint = this.$route.meta.endpoint;
     if (this.endpoint !== undefined) {
-      this.fetchData(this.endpoint)
+      this.fetchData(this.endpoint);
     }
     // set dataLoaded flag for routes with no data fetching
     if (this.$route.name === 'favorites' || this.$route.name === '404') {
-      this.dataLoaded = true
+      this.dataLoaded = true;
     }
   },
 
   watch: {
     '$route' (to, from) {
-      this.isHome()
-      this.isSearch()
+      this.isHome();
+      this.isSearch();
 
-      this.endpoint = this.$route.meta.endpoint
-      let fetch = this.doFetch(to, from)
+      this.endpoint = this.$route.meta.endpoint;
+      const fetch = this.doFetch(to, from);
 
       if (this.endpoint !== undefined && fetch !== false) {
-        this.data = null
-        this.dataLoaded = false
-        this.fetchData(this.endpoint)
+        this.data = null;
+        this.dataLoaded = false;
+        this.fetchData(this.endpoint);
       }
     }
   },
 
   methods: {
+    afterEnter (el) {
+      el.classList.remove(this.transitionEnter);
+    },
+
+    afterLeave (el) {
+      el.classList.remove(this.transitionLeave);
+    },
+
     beforeEnter (el) {
+      console.log('beforeEnter this.direction', this.direction);
+
       switch (this.direction) {
         case 'forward':
-          this.transitionEnter = 'slide-in-right'
+          this.transitionEnter = 'slide-in-right';
           break
         case 'back':
-          this.transitionEnter = 'slide-in-left'
-          break
+          this.transitionEnter = 'slide-in-left';
+          break;
         default:
-          this.transitionEnter = 'slide-in-up'
+          this.transitionEnter = 'slide-in-up';
+          break;
       }
-      el.classList.add(this.transitionEnter)
+      el.classList.add(this.transitionEnter);
 
       // reset scroll position for iOS
       setTimeout(() => {
         window.scrollTo(0, 1)
-      }, 0)
-    },
-
-    afterEnter (el) {
-      el.classList.remove(this.transitionEnter)
+      }, 0);
     },
 
     beforeLeave (el) {
+      console.log('beforeLeave this.direction', this.direction);
+
       switch (this.direction) {
         case 'forward':
-          this.transitionLeave = 'slide-out-left'
-          break
+          this.transitionLeave = 'slide-out-left';
+          break;
         case 'back':
-          this.transitionLeave = 'slide-out-right'
-          break
+          this.transitionLeave = 'slide-out-right';
+          break;
         default:
-          this.transitionLeave = 'slide-out-down'
+          this.transitionLeave = 'slide-out-down';
+          break;
       }
-      el.classList.add(this.transitionLeave)
-    },
-
-    afterLeave (el) {
-      el.classList.remove(this.transitionLeave)
-    },
-
-    fetchData (endpoint) {
-      let endpointUrl = this.$root.apiBaseUrl + endpoint
-      let slug = this.$route.params.slug
-
-      // append query for search
-      if (window.location.search) {
-        endpointUrl += window.location.search
-      }
-
-      // set slug to undefined when going from 'page' to 'info'
-      if (slug === null) {
-        slug = undefined
-      }
-
-      if (slug !== undefined) {
-        endpointUrl += '/' + slug
-      }
-
-      let that = this
-      request
-      .get(endpointUrl)
-      .end((error, response) => {
-        if (error || !response.ok) {
-          that.notFound()
-        } else {
-          that.data = response.body
-          that.$store.commit('setParent', response.body.parent)
-          that.updateTitle(response.body.title)
-          that.dataLoaded = true
-        }
-      })
+      el.classList.add(this.transitionLeave);
     },
 
     doFetch(to, from) {
       if (to.name === 'intro') {
         if (from.name === 'steps' || from.name === 'ingredients') {
-          return false
+          return;
         }
       } else if (to.name === 'steps' || to.name === 'ingredients') {
-        return false
+        return;
       } else if (from.name === 'pages' && to.name === 'chapters') {
-        return false
+        return;
       } else if (from.name === 'chapters' && to.name === 'pages') {
-        return false
+        return;
       } else if (to.name === 'favorites') {
-        return false
+        return;
       } else {
-        return true
+        return true;
       }
     },
 
-    updateTitle (title) {
-      document.title = (title !== undefined) ? `${title} · ${this.$root.siteName}` : this.$root.siteName
+    fetchData (endpoint) {
+      let endpointUrl = this.$root.apiBaseUrl + endpoint;
+      let slug = this.$route.params.slug;
+
+      // append query for search
+      if (window.location.search) {
+        endpointUrl += window.location.search;
+      }
+
+      // set slug to undefined when going from 'page' to 'info'
+      if (slug === null) {
+        slug = undefined;
+      }
+
+      if (slug !== undefined) {
+        endpointUrl += '/' + slug;
+      }
+
+      let that = this;
+      request
+      .get(endpointUrl)
+      .end((error, response) => {
+        if (error || !response.ok) {
+          that.notFound();
+        } else {
+          that.data = response.body;
+          that.$store.commit('setParent', response.body.parent);
+          that.updateTitle(response.body.title);
+          that.dataLoaded = true;
+        }
+      })
     },
 
     isHome () {
-      let routeName = this.$route.name
-      this.home = (routeName === 'chapters') ? true : false
+      this.home = (this.$route.name === 'chapters');
     },
 
     isSearch () {
-      let routeName = this.$route.name
-      let setSearch = (routeName === 'search') ? true : false
-      this.$store.commit('setSearch', setSearch)
+      const setSearch = (this.$route.name === 'search');
+      this.$store.commit('setSearch', setSearch);
     },
 
     notFound() {
-      window.location.replace('/404')
-    }
-  }
+      window.location.replace('/404');
+    },
+
+    updateTitle (title) {
+      document.title = (title !== undefined) ? `${title} · ${this.$root.siteName}` : this.$root.siteName;
+    },
+  },
 }
 </script>
 
