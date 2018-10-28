eeab8b4b5f3cdb489f1573a2e36e9807fc62941c
Ben Gauslin <ben@gauslin.com>
2018-10-28 17:40:08 -0400

    goodandcheap migrated from laravel-elixir to vanilla gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,106 @@
-var elixir = require('laravel-elixir');
+const pkg = require('./package.json');
 
-require('laravel-elixir-browserify-official');
-require('laravel-elixir-vueify');
-require('laravel-elixir-pug');
-require('laravel-elixir-livereload');
+const gulp         = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const babelify     = require('babelify');
+const browserify   = require('browserify');
+const browserSync  = require('browser-sync');
+const buffer       = require('vinyl-buffer');
+const cssnano      = require('gulp-cssnano');
+const hash         = require('gulp-hash');
+const plumber      = require('gulp-plumber');
+const rename       = require("gulp-rename");
+const source       = require('vinyl-source-stream');
+const stylus       = require('gulp-stylus');
+const uglify       = require('gulp-uglify-es').default;
+const vueify       = require('vueify');
 
-elixir.config.sourcemaps = false;
-elixir.config.assetsPath = 'source';
-elixir.config.js.outputFolder = 'ui';
-elixir.config.css.outputFolder = 'ui';
+const onError = (err) => console.log(err);
 
-elixir(mix => {
-  mix.browserify('goodandcheap.js')
-  .stylus('goodandcheap.styl')
-  .stylus('offline.styl')
-  .copy('source/html', 'public')
-  .copy('source/img', 'public/ui/img')
-  .copy('source/webfonts', 'public/ui/webfonts')
-  .version([
-    'public/ui/goodandcheap.js',
-    'public/ui/goodandcheap.css'
-  ])
-  .livereload()
-  .browserSync({
-    proxy: 'goodandcheap.test'
+// ------------------------------------------------------------
+// Individual tasks.
+
+// Copy html.
+gulp.task('html', () => {
+  gulp.src(pkg.paths.html.src)
+    .pipe(gulp.dest(pkg.paths.html.dest));
+});
+
+// Copy image assets.
+gulp.task('img', () => {
+  gulp.src(pkg.paths.img.src)
+    .pipe(gulp.dest(pkg.paths.img.dest));
+});
+
+// Compile and uglify JavaScript.
+// https://gist.github.com/alkrauss48/a3581391f120ec1c3e03
+gulp.task('js', () => {
+  return browserify({ entries: pkg.paths.js.src, debug: true })
+    .transform('babelify', { presets: ['@babel/preset-env'] })
+    .transform('vueify')
+    .bundle()
+    .pipe(source(pkg.paths.js.bundleSrc))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest(pkg.paths.js.bundleDest));
+});
+
+// Compile and minify stylus.
+gulp.task('stylus', () => {
+  gulp.src(pkg.paths.stylus.src)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(stylus())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
+    .pipe(cssnano({
+      discardUnused: false,
+      minifyFontValues: false,
+    }))
+    .pipe(gulp.dest(pkg.paths.stylus.dest));
+});
+
+// Copy webfonts.
+gulp.task('webfonts', () => {
+  gulp.src(pkg.paths.webfonts.src)
+    .pipe(gulp.dest(pkg.paths.webfonts.dest));
+});
+
+// ------------------------------------------------------------
+// Composite tasks (in order of operation).
+
+gulp.task('browser-sync', ['watch'], () => {
+  return browserSync({ proxy: pkg.devServer });
+});
+
+gulp.task('watch', pkg.tasks.default, () => {
+  const watcher = gulp.watch('./source/**/*', ['refresh']);
+  watcher.on('change', (event) => {
+    console.log(`File ${event.path} was ${event.type}, running tasks...`);
   });
 });
+
+gulp.task('refresh', pkg.tasks.default, browserSync.reload);
+
+// ------------------------------------------------------------
+// Primary tasks.
+
+// One-time build.
+gulp.task('build', pkg.tasks.build, () => console.log('Build completed.'));
+
+// Build, listen, reload.
+gulp.task('default', ['browser-sync']);
+
+// Create hashed files for production.
+gulp.task('version', () => {
+  gulp.src(pkg.paths.version.src)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(hash())
+    .pipe(gulp.dest(pkg.paths.version.dest))
+    .pipe(hash.manifest(pkg.paths.version.manifestFile, {
+      deleteOld: true,
+    }))
+    .pipe(gulp.dest('.'));
+});
+
