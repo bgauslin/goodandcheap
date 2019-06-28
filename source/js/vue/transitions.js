71604a5bbd72b2ac4beb051147b2689cec39aab4
Ben Gauslin <ben@gauslin.com>
2019-06-28 07:36:52 -0400

    make transitions.js DRY

--- source/js/vue/transitions.js
+++ source/js/vue/transitions.js
@@ -1,40 +1,38 @@
 export default (router) => {
 
   router.beforeEach((to, from, next) => {
-    // chapters -> chapter
+    let direction;
+
+    // chapters => chapter
     if (from.name === 'chapters' && to.name === 'chapter') {
-      router.app.$store.commit('direction', 'forward');
-
-    // chapter -> chapters
+      direction = 'forward';
+    // chapter => chapters
     } else if (from.name === 'chapter' && to.name === 'chapters') {
-      router.app.$store.commit('direction', 'back');
-
-    // pages -> info
+      direction = 'back';
+    // pages => info
     } else if (from.name === 'pages' && to.name === 'info') {
-      router.app.$store.commit('direction', 'forward');
-
-    // info -> pages
+      direction = 'forward';
+    // info => pages
     } else if (from.name === 'info' && to.name === 'pages') {
-      router.app.$store.commit('direction', 'back');
-
-    // chapter -> recipe
+      direction = 'back';
+    // chapter => recipe
     } else if (from.name === 'chapter' && to.name === 'intro') {
-      router.app.$store.commit('direction', 'forward');
-
+      direction = 'forward';
     } else if (from.name === 'intro' || from.name === 'ingredients' || from.name === 'steps' ) {
-      // recipe -> chapter
+      // recipe => chapter
       if (to.name === 'chapter') {
-        router.app.$store.commit('direction', 'back');
+        direction = 'back';
       }
       // recipe tabs
       if (to.name === 'intro' || to.name === 'ingredients' || to.name === 'steps' ) {
-        router.app.$store.commit('direction', null);
+        direction = null;
       }
     // default
     } else {
-      router.app.$store.commit('direction', 'up');
+      direction = 'up';
     }
 
+    router.app.$store.commit('direction', direction);
     next();
   });
 }
