62a3337aa470e13aa917d5d3c40f8db52dccddfe
Ben Gauslin <github@gauslin.com>
2019-10-13 10:23:24 -0400

    Cleans up goodandcheap.js entry file

--- src/js/goodandcheap.js
+++ src/js/goodandcheap.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 import setup from './setup';
 import Vue from 'vue/dist/vue.js';
 import App from './vue/components/App.vue';
@@ -6,10 +7,9 @@ import router from './vue/router';
 import store from './vue/store/index';
 import transitions from './vue/transitions';
 
-setup.init();
-
 Vue.config.productionTip = false;
 transitions(router);
+setup.init();
 
 new Vue({
   components: { App },
