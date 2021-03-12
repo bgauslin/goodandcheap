96e3593ae74332a92437822c695896d58fb4b778
Ben Gauslin <bgauslin@gmail.com>
2021-03-12 04:43:17 -0500

    updated tabs styles - fixes #40

--- src/js/vue/components/Tabs.vue
+++ src/js/vue/components/Tabs.vue
@@ -27,37 +27,34 @@ export default {
 
 .tabs
   align-items center
-  border 1px solid var(--brand-color)
-  border-radius rem(4)
+  background var(--background-0)
+  border-radius 8px
   display flex
   margin 1rem var(--margin)
-  overflow hidden
+  padding 4px
 
 .tabs__item
-  background var(--background-1)
   flex 1 1
   line-height 1
   list-style none
   margin 0
 
-.tabs__item:not(:last-child)
-  border-right 1px solid var(--brand-color)
-
 .tabs__link
-  link(var(--brand-color), var(--brand-color), var(--brand-color), var(--brand-color))
-  link_background(transparent, transparent, var(--background-0), var(--background-0))
+  link(var(--text-2-color), var(--text-2-color), var(--brand-color), var(--brand-color))
   small_caps()
   typeface('sans_bold')
+  border-radius 4px
   display block
   padding .75em 1em
   text-align center
 
 .tabs__link.current
   cursor default
+  elevation(1)
 
 .tabs__link.current
 [no-touch] .tabs__link.current:hover
-  background var(--brand-color)
-  color var(--background-1)
+  background var(--background-2)
+  color var(--text-1-color)
 
 </style>
