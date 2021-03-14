f490a5620d66f84943fdf7fd83aca23f87027801
Ben Gauslin <bgauslin@gmail.com>
2021-03-14 05:31:18 -0400

    checkbox icons with vanilla svg

--- src/js/vue/components/ChecklistItem.vue
+++ src/js/vue/components/ChecklistItem.vue
@@ -1,14 +1,36 @@
 <template lang="pug">
   li.checklist__item
-    input.checklist__checkbox(
-      type="checkbox"
-      :id="itemId"
-      :checked="isSaved"
-      @click="toggleItem()"
-    )
     label.checklist__label(
       :for="itemId"
-    ) {{ item }}
+    )
+      input.checklist__checkbox(
+        type="checkbox"
+        :id="itemId"
+        :checked="isSaved"
+        @click="toggleItem()"
+      )
+      svg(
+        :class="['checklist__icon', isSaved ? 'checklist__icon--checked' : 'checklist__icon--unchecked']"
+        viewbox="0 0 24 24"
+        aria-hidden="true"
+      )
+        circle(
+          v-if="!isSaved"
+          cx="12"
+          cy="12"
+          r="10"
+        )
+        circle(
+          v-if="isSaved"
+          cx="12"
+          cy="12"
+          r="12"
+        )
+        polyline(
+          v-if="isSaved"
+          points="5,12 10,17 19,8"
+        )
+      template {{ item }}
 </template>
 
 <script>
@@ -64,34 +86,42 @@ export default {
 checkbox-size = rem(24)
 
 .checklist__item
-  align-items center
-  display flex
   margin 0
+
+.checklist__label
+  link(var(--text-1-color), var(--text-1-color), var(--text-2-color), var(--text-2-color))
+  align-items center
+  cursor pointer
+  display inline-flex
+  padding rem(8) rem(4)
+  user-select none
 
 .checklist__checkbox
   appearance none
   border 0
-  flex 0 0 checkbox-size
-  height checkbox-size
-  margin rem(8) 0
+  flex 0 0
+  height 0
+  margin 0
   outline 0
 
-.checklist__checkbox
-.checklist__label
-  cursor pointer
+.checklist__icon
+  height rem(24)
+  margin-right rem(8)
+  width rem(24)
 
-.checklist__checkbox::before
-  icon(icon-circle-empty)
-  color var(--text-3-color)
-  display block
-  font-size rem(22)
+.checklist__icon--unchecked
+  & > circle
+    fill none
+    stroke var(--text-3-color)
+    stroke-width 3
 
-.checklist__checkbox:checked::before
-  color var(--checked-color)
-  content icon-ok-circled
+.checklist__icon--checked
+  & > circle
+    fill var(--checked-color)
 
-.checklist__label
-  link(var(--text-1-color), var(--text-1-color), var(--text-2-color), var(--text-2-color))
-  padding rem(8) rem(4)
+  & > polyline
+    fill none
+    stroke var(--background-1)
+    stroke-width 3
 
 </style>
