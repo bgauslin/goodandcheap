7ed98674ff30d49411a6a0a8aa81b96b7be89041
Ben Gauslin <github@gauslin.com>
2025-10-08 09:42:47 -0300

    initial commit with scaffolding and prior assets

---
+++ src/js/components/app/app.ts
@@ -0,0 +1,28 @@
+import {LitElement, css, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+import shadowStyles from './app.scss';
+
+/**
+ * Web component for a clock with nine sets of hands.
+ */
+@customElement('goodandcheap-app')
+class App extends LitElement {
+  static styles = css`${shadowStyles}`;
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+
+  protected render() {
+    return html``;
+  }
+}
---
+++ webpack.common.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const FontPreloadPlugin = require('webpack-font-preload-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/js/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/root' },
+      ],
+    }),
+    new FontPreloadPlugin({
+      index: 'index.html',
+      insertBefore: 'link:first-of-type',
+      loadType: 'preload',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/html/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        // Encapsulated shadow DOM styles in web components.
+        test: /\.scss$/,
+        include: [
+          path.resolve(__dirname, 'src/js/components')
+        ],
+        use: [
+          'lit-css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              api: 'modern',
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+            },
+          },
+        ],
+      },
+      {
+        // App shell styles and custom properties.
+        test: /\.scss$/,
+        include: [
+          path.resolve(__dirname, 'src/styles')
+        ],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              api: 'modern',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.scss'],
+  },
+}
