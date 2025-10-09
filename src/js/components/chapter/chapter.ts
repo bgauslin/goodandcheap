5be29c6f1fa743e935895506a4708673cd0abb12
Ben Gauslin <github@gauslin.com>
2025-10-09 13:14:19 -0300

    chapter image; props by alpha

--- src/js/components/chapter/chapter.ts
+++ src/js/components/chapter/chapter.ts
@@ -3,15 +3,15 @@ import {customElement, property, state} from 'lit/decorators.js';
 import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 
 interface chapter {
-  title: string,
   content: string,
   image: string,
   recipes: {
-    title: string,
     chapter: string,
-    slug: string,
     image: string,
+    slug: string,
+    title: string,
   }[],
+  title: string,
 }
 
 @customElement('gc-chapter')
@@ -62,13 +62,15 @@ class Chapter extends LitElement {
     return html`
       <h1>${title}</h1>
       ${unsafeHTML(content)}
-      <img src="${image}" alt="">
+      <img src="/images/${image}@large.webp" alt="">
       <ul>
       ${recipes.map(recipe => {
-        const {image, slug, title} = recipe;
+        const {chapter, image, slug, title} = recipe;
         return html`
           <li>
-            <a href="${slug}" data-type="recipe">
+            <a href="${slug}"
+              data-context="${chapter}"  
+              data-type="recipe">
               <img src="/images/${image}@thumb.webp" alt="">
               ${title}
             </a>
