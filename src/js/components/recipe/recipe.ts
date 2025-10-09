4b97dc40a58e25f93ff5703d9c6111353ce32aae
Ben Gauslin <github@gauslin.com>
2025-10-09 13:15:02 -0300

    recipe - props by alpha; fixes section wrappers

--- src/js/components/recipe/recipe.ts
+++ src/js/components/recipe/recipe.ts
@@ -3,27 +3,28 @@ import {customElement, property, state} from 'lit/decorators.js';
 import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 
 interface recipe {
-  title: string,
   badge: string,
-  image: string,
   cost?: {
-    total: string,
     each: string,
+    total: string,
     units: string
   },
-  overview: string,
+  image: string,
   ingredients?: {
-    label?: string,
     items: string[],
+    label?: string,
   }[],
+  overview: string,
   steps?: string[],
+  title: string,
 }
 
 @customElement('gc-recipe')
 class Recipe extends LitElement {
   @property() recipe: string;
-  @state() endpoint: string;
+
   @state() data: recipe;
+  @state() endpoint: string;
 
   constructor() {
     super();
@@ -64,48 +65,39 @@ class Recipe extends LitElement {
 
     const {badge, cost, image, ingredients, overview, steps, title} = this.data;
 
-    const cost_ = cost ?
-      html`
-        <p class="cost">
-          ${cost.total} Total<br>
-          ${cost.each} / ${cost.units}
-        </p>
-      ` : nothing;
+    const cost_ = cost ? html`
+      <p class="cost">
+        ${cost.total} Total<br>
+        ${cost.each} / ${cost.units}
+      </p>` : nothing;
 
-    const overview_ = overview ?
-      html`
-        <section class="overview">
-          <h2>Overview</h2>
-          ${unsafeHTML(overview)}
-        </section>
-      ` : nothing;
+    const overview_ = overview ? html`
+      <section class="overview">
+        <h2>Overview</h2>
+        ${unsafeHTML(overview)}
+      </section>` : nothing;
 
-    const ingredients_ = ingredients ?
-        ingredients.map((group, index) => {
-          const {label, items} = group;
-          return html`
-            ${index === 0 ?
-              html`
-                <section class="ingredients">
-                  <h2>Ingredients</h2>
-              ` : nothing}
-                  ${label ? html`<h3>${label}</h3>` : nothing}
-                  <ul>
-                    ${items.map(item => html`<li>${item}</li>`)}
-                  </ul>
-            ${index === items.length ? html`</section>` : nothing}
-          `;
-        }) : nothing;
+    const ingredients_ = ingredients ? html`
+      <section class="ingredients">
+      ${ingredients.map((group, index) => {
+        const {label, items} = group;
+        return html`
+          ${index === 0 ? html`<h2>Ingredients</h2>` : nothing}
+          ${label ? html`<h3>${label}</h3>` : nothing}
+          <ul>
+            ${items.map(item => html`<li>${item}</li>`)}
+          </ul>
+        `;
+      })}
+      </section>` : nothing;
 
-    const steps_ = steps ?
-        html`
-          <section class="steps">
-            <h2>Steps</h2>
-            <ol>
-              ${steps.map(step => html`<li>${step}</li>`)}
-            </ol>
-          </section>` :
-        nothing;
+    const steps_ = steps ? html`
+      <section class="steps">
+        <h2>Steps</h2>
+        <ol>
+          ${steps.map(step => html`<li>${step}</li>`)}
+        </ol>
+      </section>` : nothing;
 
     return html`
       <section class="cover">
