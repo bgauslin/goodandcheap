a1a5582b3c537b1d107eae8390af82c6e1c7c34c
Ben Gauslin <github@gauslin.com>
2025-10-27 15:59:20 -0300

    truncate component is working correctly now

--- src/js/truncate.ts
+++ src/js/truncate.ts
@@ -1,25 +1,21 @@
-import {LitElement, html, nothing, PropertyValues} from 'lit';
-import {customElement, property, queryAll, state} from 'lit/decorators.js';
+import {LitElement, css, html, PropertyValues} from 'lit';
+import {customElement, property, state} from 'lit/decorators.js';
 import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 
 
 /**
- * Web component that truncates text content via CSS and adds a button for
- * showing the rest of the content.
+ * Web component that truncates text content and adds a button for showing
+ * the rest of it. The original text doesn't display since there's no <slot>
+ * for it in the shadow DOM.
  */
 @customElement('gc-truncate')
 class GoodAndCheapTruncate extends LitElement {
-
   @property({attribute: 'content-id'}) contentId: string;
-  @property({reflect: true}) words: number = 30;
-  
-  @queryAll('p') paragraphElements: HTMLParagraphElement[];  
-  
+  @property({attribute: 'words', reflect: true}) wordCount: number = 30;
+    
   @state() enabled: boolean = true;
-  @state() showButton: boolean = false;
-  
-  @state() paragraphs: string[];
   @state() original: string;
+  @state() paragraphs: string[];
   @state() truncated: string;
 
   constructor() {
@@ -34,10 +30,6 @@ class GoodAndCheapTruncate extends LitElement {
     super.disconnectedCallback();
   }
 
-  protected createRenderRoot() {
-    return this;
-  }
-
   protected willUpdate(changedProperties: PropertyValues<this>) {
     if (changedProperties.has('contentId')) {
       this.enabled = true;
@@ -48,27 +40,41 @@ class GoodAndCheapTruncate extends LitElement {
     }
   }
 
-  private setup() {
-    // Save original paragraph text for extracting the truncated version.
-    for (const p of this.paragraphElements) {
+  /**
+   * Saves the original paragraphs for rendering and creates a truncated
+   * version from the first paragraph.
+   */
+  private async setup() {
+    await this.updateComplete;
+
+    // Get original paragraphs in the light DOM for extracting a truncated
+    // version.
+    const paragraphElements = this.querySelectorAll('p')
+    for (const p of paragraphElements) {
       this.paragraphs.push(p.textContent);
     }
 
     // Reassemble the original paragraphs for replacing the truncated version.
-    for (const p of this.paragraphElements) {
+    for (const p of paragraphElements) {
       this.original += `<p>${p.textContent}</p>`;
     }
 
-    // Clear everything out.
-    // this.innerHTML = '';
-    
-    // Assemble the truncated version.
+    // Create the truncated version, but only if there are enough words to
+    // show when expanded. I.e., it's silly to hide only 2-3 more words, so
+    // set a threshold for truncating the original in addition to the target
+    // word count.
+    const threshold = this.wordCount / 2;
     const words = this.paragraphs[0].split(/\s+/);
-    for (let i = 0; i < this.words; i++) {
-      this.truncated += `${words[i]}`;
-      if (i < this.words - 1) {
-        this.truncated += ' ';
+
+    if (words.length >= (this.wordCount + threshold)) {
+      for (let i = 0; i < this.wordCount; i++) {
+        this.truncated += `${words[i]}`;
+        if (i < this.wordCount - 1) {
+          this.truncated += ' ';
+        }
       }
+    } else {
+      this.enabled = false;
     }
   }
 
@@ -84,4 +90,23 @@ class GoodAndCheapTruncate extends LitElement {
       </p>` : html`${unsafeHTML(this.original)}`}
     `;
   }
+
+  // Shadow DOM stylesheet.
+  static styles = css`
+    p {
+      line-height: var(--base-line-height);
+    }
+
+    button {
+      appearance: none;
+      background: none;
+      border: none;
+      color: var(--brand-color);
+      cursor: pointer;
+      font: inherit;
+      margin: 0;  
+      outline: none;
+      padding: 0;
+    }
+  `;
 }
